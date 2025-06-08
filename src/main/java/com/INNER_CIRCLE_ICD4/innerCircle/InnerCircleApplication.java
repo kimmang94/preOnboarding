9c6d5d387f86f02511d5f392fc6e7377978bf4e2
kimmang94 <kimmang940715@naver.com>
2025-06-08 23:55:21 +0900

    온보딩 사전 연습 : localhost:8080 활성화 확인 [O] DI 추가 [O]

---
+++ src/main/java/com/INNER_CIRCLE_ICD4/innerCircle/InnerCircleApplication.java
@@ -0,0 +1,13 @@
+package com.INNER_CIRCLE_ICD4.innerCircle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnerCircleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnerCircleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/INNER_CIRCLE_ICD4/innerCircle/InnerCircleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.INNER_CIRCLE_ICD4.innerCircle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnerCircleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
